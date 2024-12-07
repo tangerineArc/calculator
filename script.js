4388d45f2e58bd015090b8049ea13a6116146a19
Swagatam <swagatamheroz@outlook.com>
2024-12-07 18:40:45 +0530

    feat: achieve display updation

--- script.js
+++ script.js
@@ -3,43 +3,71 @@
 const secondaryDisplay = document.querySelector(".secondary-display");
 const primaryDisplay = document.querySelector(".primary-display");
 
-const buttons = document.querySelectorAll(".buttons > div > button");
-// console.log(buttons);
+setButtonListeners();
 
-buttons.forEach(button => {
-    button.addEventListener("click", event => {
-        operate(event.target.id);
+function setButtonListeners() {
+    const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+    const binaryOperations = ["/", "*", "-", "+"];
+    const unaryOperations = ["all-clear", "negation", "percentage"];
+
+    document.querySelectorAll(".buttons > div > button").forEach(button => {
+        button.addEventListener("click", event => {
+            if ((event.target.textContent === "-" && !primaryDisplay.textContent) || digits.includes(event.target.textContent)) {
+                primaryDisplay.textContent = primaryDisplay.textContent.concat(event.target.textContent);
+            } else if (primaryDisplay.textContent && binaryOperations.includes(event.target.textContent)) {
+                const secondaryDisplayText = secondaryDisplay.textContent.trim();
+                if (binaryOperations.includes(secondaryDisplayText[secondaryDisplayText.length - 1])) {
+                    secondaryDisplay.textContent = calculate()
+                    secondaryDisplay.textContent = secondaryDisplay.textContent.concat(` ${event.target.textContent} `);
+                    primaryDisplay.textContent = "";
+                } else {
+                    secondaryDisplay.textContent = secondaryDisplayText
+                        .concat(primaryDisplay.textContent)
+                        .concat(` ${event.target.textContent} `);
+                    primaryDisplay.textContent = "";
+                }
+            } else if (unaryOperations.includes(event.target.id)) {
+                const res = operateOnUnary(event.target.id);
+                if (res) {
+                    primaryDisplay.textContent = res;
+                }
+            } else if (event.target.id === "equals") {
+                primaryDisplay.textContent = calculate();
+                secondaryDisplay.textContent = "";
+            } else if (event.target.id === "delete") {
+                primaryDisplay.textContent = primaryDisplay.textContent.slice(0, primaryDisplay.textContent.length - 1);
+            }
+        });
     });
-});
+}
 
-function operate(type) {
-    let res;
+function operateOnUnary(type) {
     switch (type) {
         case "all-clear":
-            clear();
-            break;
+            return clear();
         case "negation":
-            res = negate(primaryDisplay.textContent);
-            break;
+            return negate(primaryDisplay.textContent);
         case "percentage":
-            res = percentage(primaryDisplay.textContent);
-            break;
-        case "addition":
-            res = add(secondaryDisplay.textContent, primaryDisplay.textContent);
-            break;
-        case "subtraction":
-            res = subtract(secondaryDisplay.textContent, primaryDisplay.textContent);
-            break;
-        case "multiplication":
-            res = multiply(secondaryDisplay.textContent, primaryDisplay.textContent);
-            break;
-        case "division":
-            res = divide(secondaryDisplay.textContent, primaryDisplay.textContent);
-            break;
+            return percentage(primaryDisplay.textContent);
     }
+}
 
-    console.log(res, typeof res);
-    return res;
+function calculate() {
+    const [operand1, operator] = secondaryDisplay.textContent.trim().split(" ");
+    const operand2 = primaryDisplay.textContent;
+
+    if (!operand2) return operand1;
+    
+    switch (operator) {
+        case "+":
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        case "/":
+            return divide(operand1, operand2);
+    }
 }
 
 function add(num1, num2) {
